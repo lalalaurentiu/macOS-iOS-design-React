b078ec35c471a5213da5f635b0846b1bc74e42b5
lalalaurentiu <laurentiumarianbaluta@gmail.com>
2023-02-06 10:14:52 +0200

    adding navbar bluetooth elements

--- src/components/header/navcomponents.js
+++ src/components/header/navcomponents.js
@@ -4,6 +4,8 @@ import Wifi from "../wifi";
 import DateTime from "../DateTime";
 import { nanoid } from "nanoid";
 import NaviconGo from "./navicon-GO";
+import OnOffButton from "./onoffbutton";
+import Icon from "./icon";
 
 const navcomponentsLeft = [
   {
@@ -1391,7 +1393,36 @@ const navcomponentsRight = [
     name: {
       logoUrl: "/images/bluetooth.svg",
     },
-    subSection: [],
+    subSection: [
+      {
+        id: nanoid(),
+        name: "Bluetooth",
+        img: [
+          {
+            id: nanoid(),
+            name: <OnOffButton />,
+          },
+        ],
+        break: true,
+      },
+      {
+        id: nanoid(),
+        name: React.createElement("span", { style: {fontSize:"10px"} }, "Devices"),
+      },
+      {
+        id: nanoid(),
+        name: <Icon url="/images/bluetooth.svg" name="Apple Bluetooth Mouse" />,
+      },
+      {
+        id: nanoid(),
+        name: <Icon url="/images/airpods.svg" name="Apple AirPods" />,
+        break: true,
+      },
+      {
+        id: nanoid(),
+        name: "Bluetooth Preferences...",
+      },
+    ],
   },
   {
     id: nanoid(),
